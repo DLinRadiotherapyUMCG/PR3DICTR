d16e8de0053a8b08c8c6ff915c5791e4195ab654
Daniel MacRae <61056860+daniel-macrae@users.noreply.github.com>
2024-11-27 08:43:34 +0100

    delete old code (again)

--- SIMS/code L ref/test.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 13 10:53:31 2024
-
-@author: HoekL02
-"""
-
--- SIMS/hnc-ensemble-master/src/utils/log_table.py
+++
@@ -1,9 +0,0 @@
-from rich.console import Console
-from rich.text import Text
-
-
-def log_table(rich_table):
-    console = Console()
-    with console.capture() as capture:
-        console.print(rich_table)
-    return Text.from_ansi(capture.get())
